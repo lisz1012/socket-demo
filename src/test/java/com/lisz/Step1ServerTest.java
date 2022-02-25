9b2cc519102462c01296c5ac5ce318bc28fcff03
lisz1012 <lisz1012@163.com>
2022-02-25 10:51:43 -0800

    Add test

---
+++ src/test/java/com/lisz/Step1ServerTest.java
@@ -0,0 +1,12 @@
+package com.lisz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Step1ServerTest {
+	@Test
+	public void testListen() {
+		System.out.println("Testing...");
+		Assert.assertEquals("aaa", "aaa");
+	}
+}
